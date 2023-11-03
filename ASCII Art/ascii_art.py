ce8e68983401a7d23740861adb8eafb9d8eeabd0
Fidel Korti-Frost <148016801+korti-frost@users.noreply.github.com>
2023-11-03 20:39:16 +0100

    finished ascii_art.py

--- ASCII Art/ascii_art.py
+++ ASCII Art/ascii_art.py
@@ -7,7 +7,22 @@ t = input()
 t = t.upper()
 atoz = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 atoz_ascii = []
+char_pos = -1
+answer = []
 
 for i in range(h):
     row = input()
     atoz_ascii.append([row[i:i+l] for i in range(0, len(row), l)])
+
+for i in range(h):
+    row = ""
+    for char in t:
+        char_pos = atoz.find(char)
+        if (char_pos == -1):
+            row += str(atoz_ascii[i][26])
+        else:
+            row += str(atoz_ascii[i][char_pos])
+    answer.append(row)
+
+for i in answer:
+    print(str(i))
